01c4174c60b5051ed46417c5cf9b229a4148fd65
Vlad <50659238+VladLiteanu@users.noreply.github.com>
2021-04-12 18:10:09 +0200

    Add axes support

--- front_end/Scripts.js
+++ front_end/Scripts.js
@@ -5,7 +5,6 @@ background.addEventListener('keydown', (e)=> {
 })
 
 
-
 function updateGamepad(){
     requestAnimationFrame(updateGamepad)
     let gp = navigator.getGamepads()[0];
@@ -15,7 +14,17 @@ function updateGamepad(){
                 console.log(button, gp.buttons.indexOf(button))
             }
         })
+        if(gp.axes[0] > 0.15 || gp.axes[0] < -0.15) {
+            console.log(gp.axes[0], "left X");
+          } else if(gp.axes[1] > 0.15 || gp.axes[1] < -0.15) {
+            console.log(gp.axes[1], "left Y (inverted)");
+          } else if(gp.axes[2] > 0.15 || gp.axes[2] < -0.15) {
+            console.log(gp.axes[2], "right X");
+          } else if(gp.axes[3] > 0.15 || gp.axes[3] < -0.15) {
+            console.log(gp.axes[3], "right Y (inverted)");
+          }
     }
 }
+
 
 updateGamepad()
