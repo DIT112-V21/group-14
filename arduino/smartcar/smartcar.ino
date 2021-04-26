849ae825a008f5c8f585b196f0d826a9bff91cef
Vlad <vlad.george.liteanu@gmail.com>
2021-04-26 13:35:40 +0200

    Add camera to car
    Implemented camera to car

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,36 +1,41 @@
 #include <Smartcar.h>
 #include <MQTT.h>
 #include <WiFi.h>
+#ifdef __SMCE__
+#include <OV767X.h>
+#include <vector>
+std::vector<char> frameBuffer;
+#endif
 
 #ifndef __SMCE__
 WiFiClient net;
 #endif
 MQTTClient mqtt;
 
-const int TRIGGER_PIN           = 6; 
-const int ECHO_PIN              = 7; 
-const unsigned int MAX_DISTANCE = 200;
-const int FRONT_PIN = 0;
-const auto oneSecond = 1000UL;
-
-
 ArduinoRuntime arduinoRuntime;
-SR04 front(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-GP2Y0A21 frontIR(arduinoRuntime, FRONT_PIN); 
 BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
 DifferentialControl control(leftMotor, rightMotor);
-
 SimpleCar car(control);
 
-void setup()
-{
+const int TRIGGER_PIN = 6; 
+const int ECHO_PIN = 7; 
+const unsigned int MAX_DISTANCE = 200;
+const int FRONT_PIN = 0;
+const auto oneSecond = 1000UL;
+SR04 front(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+GP2Y0A21 frontIR(arduinoRuntime, FRONT_PIN); 
+
+
+void setup() {
   Serial.begin(9600);
-  #ifndef __SMCE__
+#ifndef __SMCE__
   mqtt.begin(net);
-  #else
+#else
+  Camera.begin(QVGA, RGB888, 15);
+  frameBuffer.resize(Camera.width() * Camera.height() * Camera.bytesPerPixel());
   mqtt.begin(WiFi);
-  #endif
+#endif
   if (mqtt.connect("arduino", "public", "public")) {
     mqtt.subscribe("/smartcar/control/#", 1);
     mqtt.onMessage([](String topic, String message) {
@@ -45,17 +50,26 @@ void setup()
   }
 }
 
-void loop()
-{
+void loop() {
   if (mqtt.connected()){
     mqtt.loop();
+    const auto currentTime = millis();
+#ifdef __SMCE__
+    static auto previousFrame = 0UL;
+    if (currentTime - previousFrame >= 65) {
+      previousFrame = currentTime;
+      Camera.readFrame(frameBuffer.data());
+      mqtt.publish("/smartcar/camera", frameBuffer.data(), frameBuffer.size(), false, 0);
+    }
+#endif
+    static auto previousTransmission = 0;
+    if (currentTime - previousTransmission >= oneSecond) {
+      previousTransmission = currentTime;
+      mqtt.publish("/smartcar/sensors/ultra", String(front.getDistance()));
+      mqtt.publish("/smartcar/sensors/infra", String(frontIR.getDistance()));
+    }
   }
-  static long previousTransmission = 0;
-  const auto currentTime = millis();
-  if (currentTime - previousTransmission >= oneSecond) {
-    previousTransmission = currentTime;
-    mqtt.publish("/smartcar/sensors/ultra", String(front.getDistance()));
-    mqtt.publish("/smartcar/sensors/infra", String(frontIR.getDistance()));
-  }
+#ifdef __SMCE__
   delay(10);
+#endif
 }
