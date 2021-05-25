ba77e3042ba7541192ad4a6b323c4cb981beaca2
Enis Bourbia <54246209+EnisBourbia@users.noreply.github.com>
2021-05-25 13:23:04 +0200

    Update test.js

--- front_end/test/test.js
+++ front_end/test/test.js
@@ -6,36 +6,30 @@ const app = require('electron').remote
 
 
 describe("Application launch test", function () {
-  this.timeout(20000)
-	
+	this.timeout(20000)	
 	beforeEach(function () {
-		
-    const appPath = path.join(__dirname, "../");
-
-		  this.app = new Application({
+		const appPath = path.join(__dirname, "../");
+		this.app = new Application({
 			path: electronPath,
 			args: [appPath],
 			startTimeout: 10000
   		});
-		
 		return this.app.start();      
 	});
-
  	afterEach(function () {
 		if (this.app && this.app.isRunning()) {
 			return this.app.stop();
 		}
 	});
-
 	it('Application compiles correctly.', function () {
 		return this.app.client.getWindowCount().then(function (count) {
-			assert.equal(count, 1)
+		assert.equal(count, 1)
 		})
 	})
 
 	it('Content displayed correctly.', function () {
 		return this.app.client.browserWindow.getTitle().then(function (title) {
-			assert.equal(title, 'Kachow')
+		assert.equal(title, 'Kachow')
 		})
 	});
 });
